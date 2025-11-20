373b565af2a2c935a7af8e640d7ab46d282a8441
Natali LaGuta <n.v.laguta2023@gmail.com>
2025-11-20 22:37:03 +0100

    update README

--- _back_/apps/map_points/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-class MapPointsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.map_points'
-    verbose_name = 'Точки на карте'
